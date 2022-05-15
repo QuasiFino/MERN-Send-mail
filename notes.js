49f060da06a9e96a78b5d2af1ae04fe231c4ee6c
wink-aajlak <aajlak.wink@gmail.com>
2022-05-15 08:05:25 +0530

    first commit

---
+++ notes.js
@@ -0,0 +1,17 @@
+// >yarn init
+// >yarn add express
+// >yarn add nodemon
+
+// in package.json add
+  // "engines": {
+  //   "node": "14.17.6",
+  //   "npm": "6.14.15"
+  // },
+  // "scripts": {
+  //   "start": "node index.js",
+  //   "dev": "nodemon index.js"
+  // },
+
+// >yarn run dev
+// >yarn add passport passport-google-oauth20
+
